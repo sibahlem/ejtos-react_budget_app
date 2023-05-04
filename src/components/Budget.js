2ef2123ebf8e384632c5e0c8575e4ea421c202fe
Sibahle <60863779+sibahlem@users.noreply.github.com>
2023-05-04 21:33:31 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,3 +1,6 @@
+In Budget.js you will be adding text and value for your budget. You will be importing app context and the useContext hook, and pass your AppContext to it - 
+    this is how a component connects to the context in order to get values from global state.
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
