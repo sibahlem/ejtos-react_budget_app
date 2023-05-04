1d12a40a13e44d8f69ca99c9165500eb29f61a88
Sibahle <60863779+sibahlem@users.noreply.github.com>
2023-05-04 21:51:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,3 +42,11 @@ const App = () => {
 };
 
 export default App;
+
+/* Here, you are importing different components,adding a bootstrap container that helps you center your App on the page
+
+Adding a title
+Adding a Bootstrap row
+Adding a column within the row for each of your components so far
+Imported and Rendered the AllocationForm
+Imported AppProvider and Nested components in the AppProvider element.*/
