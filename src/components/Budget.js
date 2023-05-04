c70eecc79716ad4892154f081a50a48fed6f241e
Sibahle <60863779+sibahlem@users.noreply.github.com>
2023-05-04 21:41:37 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,5 +1,5 @@
-In Budget.js you will be adding text and value for your budget. You will be importing app context and the useContext hook, and pass your AppContext to it - 
-    this is how a component connects to the context in order to get values from global state.
+/*In Budget.js you will be adding text and value for your budget. You will be importing app context and the useContext hook, and pass your AppContext to it - 
+    this is how a component connects to the context in order to get values from global state.*/
 
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
@@ -15,3 +15,5 @@ const Budget = () => {
 };
 
 export default Budget;
+
+/*Here, you are using the Bootstrap Alert classes to give a nice gray background by adding some text and hard coding a value.*/
