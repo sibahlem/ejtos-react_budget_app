9ee46e4f13e88acd6d5e94b317ba37f4fe29de4b
Sibahle <60863779+sibahlem@users.noreply.github.com>
2023-05-04 21:36:47 +0200

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,3 +1,6 @@
+In AllocationForm.js you will be creating an expense object, containing the name and the cost.
+This is what will get dispatched as the payload, and what you’ll use to update the state.
+
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
