a45adf6a8291651fa6a9cf49c58626dbe8ec768b
Sibahle <60863779+sibahlem@users.noreply.github.com>
2023-06-19 14:03:55 +0200

    some updates

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -15,10 +15,10 @@ const Currency = () => {
         <div className='alert alert-secondary' style={{backgroundColor:'#93e499'}}>
             <label style={{marginLeft: '1rem' , backgroundColor:'#93e499', color:'white'}} > Currency:
             <select name= "currency" id="currency" style={{backgroundColor:'#93e499', color:'white', border: 'none'}} onChange={(event)=> changeCurrency(event.target.value)}>
-                <option value="£">£ (Pound)</option>
-                <option value="$">$ (Dollar)</option>
-                <option value="€">€ (Euro)</option>
-                <option value="₹">₹ (Rupee)</option>
+                <option value="£" style={{color:'black'}}>£ (Pound)</option>
+                <option value="$" style={{color:'black'}}>$ (Dollar)</option>
+                <option value="€" style={{color:'black'}}>€ (Euro)</option>
+                <option value="₹" style={{color:'black'}}>₹ (Rupee)</option>
             </select>
             </label>
         </div>
